1758c3eaf739a13c1d70fa3c34298156f1a139ea
subhandp <subhan.dinda.putra@gmail.com>
2020-07-28 23:38:54 +0800

    fix wrong param promise

--- 03.js
+++ 03.js
@@ -1,12 +1,13 @@
-let f = async() => {
+(async() => {
     for (let i = 1; i <= 3; i++) {
         await setTimeout(() => {
             new Promise((res, rej) => {
                 if (i == 3) {
-                    resolve('1,2,3')
+                    res('1,2,3')
                 }
-            }).then((val) => { console.log(val) })
+            }).then((val) => {
+                console.log(`${val}, Done`);
+            })
         }, 1000);
     }
-}
-f();
+})()
